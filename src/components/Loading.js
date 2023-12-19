20a9162b59c39c67c119ae3352fcc9761fd9e0fa
qhuong1008 <qhuong1008@gmail.com>
2023-12-19 23:56:13 +0700

    create loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./css/Loading.css";
+
+const Loading = () => {
+  return (
+    <div class="content">
+      <div class="load-wrapp">
+        <div class="load-10">
+          <div class="bar"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import ProductList from '../components/ProductList';
-import sampleData from '../data/sampleData';
-import sampleProduct from '../data/sampleProduct';
-import CateSwiper from '../components/CateSwiper';
-import axios from 'axios';
-import apiConfig from '../config/apiConfig';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Layout from '../components/Layout';
-import CategoryList from '../components/CateList';
+import React, { useEffect, useState } from "react";
+import ProductList from "../components/ProductList";
+import sampleData from "../data/sampleData";
+import sampleProduct from "../data/sampleProduct";
+import CateSwiper from "../components/CateSwiper";
+import axios from "axios";
+import apiConfig from "../config/apiConfig";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
+import CategoryList from "../components/CateList";
+import Loading from "../components/Loading";
 
 const HomePage = () => {
   const [products, setProducts] = useState(sampleProduct.products);
@@ -19,24 +20,24 @@ const HomePage = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get('https://localhost:7206/collections/products?page=1'); // Replace with your API endpoint
+      const response = await axios.get(
+        "https://localhost:7206/collections/products?page=1"
+      ); // Replace with your API endpoint
       const data = await response.data;
       //console.log(data);
       setProducts(data); // Assuming the API response returns an array of product objects
     } catch (error) {
-      console.error('Error fetching products:', error);
+      console.error("Error fetching products:", error);
     }
   };
 
-  
   return (
     <div>
-      
       {/* <CateSwiper/> */}
-      <br/>
-      <br/>
-      <br/>
-      <CategoryList/>
+      <br />
+      <br />
+      <br />
+      <CategoryList />
       <ProductList products={products} numberOfItemsToShow={5} />
     </div>
   );
